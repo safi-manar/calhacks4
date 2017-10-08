688fd3667d09a3d02b531acdc7146df4660baad9
Leslie Tzeng <leslie.tzeng@berkeley.edu>
2017-10-08 00:50:25 -0700

    Switch to large binary column

--- photo_unit.py
+++ photo_unit.py
@@ -7,7 +7,7 @@ class PhotoUnit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     source_string = db.Column(db.String(120))
     trans_string = db.Column(db.String(120))
-    photo = db.Column(db.String(10000))
+    photo = db.Column(db.LargeBinary)
 
     def __init__(self, source_string, trans_string, photo):
         self.source_string = source_string
