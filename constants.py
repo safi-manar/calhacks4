4182b7d6c30ac50638d0debb4539aa149c358395
Leslie Tzeng <leslie.tzeng@berkeley.edu>
2017-10-07 22:00:28 -0700

    Before adding VisionAPI code

---
+++ constants.py
@@ -0,0 +1,3 @@
+import os
+
+vision_api_url="https://vision.googleapis.com/v1/images:annotate?key=" + os.environ['VISION_API_KEY']
--- home.py
+++ home.py
@@ -4,10 +4,19 @@ import json
 
 import requests
 from flask import Flask, request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 home = Flask(__name__)
+home.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(home)
 
 
 @home.route('/', methods=['GET'])
 def verify():
     return "Hello world", 200
+
+
+
+
+
+
