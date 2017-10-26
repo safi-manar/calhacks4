aa4f19114ad1ca88df2474f3d71f09b5aecd31c0
Leslie Tzeng <leslie.tzeng@berkeley.edu>
2017-10-26 12:32:11 -0700

    Allow source langauge to be non-English, added some more comments on PhotoUnit methods.

--- photo_unit.py
+++ photo_unit.py
@@ -38,26 +38,35 @@ class PhotoUnit(db.Model):
         labels_string = self.labels
         return json.loads(labels_string)
 
+    # Translation Methods
 
     # Given a target language, return a TranslationResponse object containing the source and target labels.
     #   If the target language does not already exist in the trans_labels dictionary, then translate_labels()
     #   will translate the labels via an API call and update the local state.
     def get_translation_response(self, source_lang, target_lang):
-        # If target translation already exists, use it.
-        trans_labels = self.get_trans_labels()
-        if target_lang in trans_labels:
-            target_labels = trans_labels[target_lang]
-        else:
-            # We have to generate this translation.
-            target_labels = self.translate_labels(target_lang)
-
-        response = TranslationResponse(self.labels, target_labels)
+        target_labels = self.translate_labels(target_lang)
+        source_labels = self.translate_labels(source_lang)
+        response = TranslationResponse(source_labels, target_labels)
         return response
 
+    # Main function for getting labels in a particular language.
+    # Defaults to cached values, otherwise goes to Google
+    def translate_labels(self, lang):
+        trans_labels = self.get_trans_labels()
+        # Labels are in english by default.
+        if lang == 'en':
+            return self.labels
+        # If translation already exists, use it.
+        elif lang in trans_labels:
+            return trans_labels[lang]
+        else:
+            # We have to generate this translation.
+            return self.translate_new_labels(lang)
 
+    # This should called when translation for target_lang has not been performed before.
     # Given a target language, update the local translation dictionary, and return a list of the labels translated
     #   in the target language.
-    def translate_labels(self, target_lang):
+    def translate_new_labels(self, target_lang):
         target_labels = translation_client.translate_text(self.get_labels(), target=target_lang)
 
         trans_labels = self.get_trans_labels()
